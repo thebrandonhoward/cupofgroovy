19f22854d1539b566bb747e6fd26b00033b974bd
thebrandonhoward <thebrandonhoward@outlook.com>
2024-08-24 17:00:09 -0500

    Adds fundamentals

--- src/hello_world.groovy
+++ src/hello_world.groovy
@@ -1,15 +1,31 @@
 def greeting = "Hello, World"
 
 greet(greeting)
-info()
+typeInfo()
+stringInfo()
+overloadingInfo()
 
 void greet(String greeting) {
     println greeting;
 }
 
-void info() {
+void typeInfo() {
     println 3.getClass().getName()
     println 333333.getClass().getName()
     println 33333333333333333.getClass().getName()
     println 3333333333333333333333333333333333333.getClass().getName()
+}
+
+void stringInfo() {
+    println "1 + 1 = ${1 + 1}"
+    println '1 + 1 = ${1 + 1}'
+}
+
+void overloadingInfo() {
+    def value = "This is groovy."
+
+    println value + "."
+    println value - "is "
+    println value + '.'
+    println value - 'is '
 }
